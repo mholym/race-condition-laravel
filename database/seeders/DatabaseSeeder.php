7f9d16d22872ec8c6d826f3507fa1da1de43dbeb
Miroslav Holý <mpuco1@azet.sk>
2022-10-31 21:24:36 +0100

    Implemented database seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Answer;
+use App\Models\Cart;
+use App\Models\Coupon;
+use App\Models\Poll;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +20,67 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::create([
+             'name' => 'Admin',
+             'email' => 'admin@admin.sk',
+             'password' => Hash::make('adminadmin'),
+         ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         Cart::create([
+             'user_id' => 1,
+             'total_discount' => 0,
+         ]);
+
+         Coupon::create([
+             'discount' => 25,
+             'code' => 'HESOYAM',
+             'expired_at' => '2023-10-29 19:16:07',
+         ]);
+
+         Coupon::create([
+             'discount' => 10,
+             'code' => 'AEZAKMI',
+             'expired_at' => '2025-10-29 19:27:12',
+         ]);
+
+         Poll::create([
+             'user_id' => 1,
+             'text' => 'Who should be our next president?',
+         ]);
+
+         Poll::create([
+             'user_id' => 1,
+             'text' => 'What food do you prefer?',
+         ]);
+
+         Answer::create([
+             'poll_id' => 2,
+             'text' => 'Human meat',
+         ]);
+
+        Answer::create([
+            'poll_id' => 2,
+            'text' => 'Krokodil meat',
+        ]);
+
+        Answer::create([
+            'poll_id' => 2,
+            'text' => 'Kanye memes',
+        ]);
+
+        Answer::create([
+            'poll_id' => 1,
+            'text' => 'Abrakadabrus',
+        ]);
+
+        Answer::create([
+            'poll_id' => 1,
+            'text' => 'Voldemorto',
+        ]);
+
+        Answer::create([
+            'poll_id' => 1,
+            'text' => 'Tutanchamonus',
+        ]);
     }
 }
